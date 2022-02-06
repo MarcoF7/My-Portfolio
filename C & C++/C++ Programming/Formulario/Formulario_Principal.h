30b971ee3b22bfa46753a0e6f94294fa2f76486f
Marco Farfan <32118875+MarcoF7@users.noreply.github.com>
2022-02-06 13:15:01 +0100

    Add files via upload

---
+++ C & C++/C++ Programming/Formulario/Formulario_Principal.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace Formulario {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de Formulario_Principal
+	/// </summary>
+	public ref class Formulario_Principal : public System::Windows::Forms::Form
+	{
+	public:
+		Formulario_Principal(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén utilizando.
+		/// </summary>
+		~Formulario_Principal()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variable del diseñador requerida.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido del método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,400);
+			this->Text = L"Formulario_Principal";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
